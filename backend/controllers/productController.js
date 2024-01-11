f1a441359f538fcd1595bc54d35fa4a6aa4889a3
NancyPatel11 <nancypatel5757@gmail.com>
2024-01-11 19:04:59 +0530

    added product controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,26 @@
+const asyncHandler = require("../middlewares/asyncHandler");
+const Product = require("../models/productModel");
+
+// @desc   Fetch all products
+// @route  GET /api/products
+// @access Public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+// @desc   Fetch a product
+// @route  GET /api/products/:id
+// @access Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    return res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+});
+
+module.exports = { getProducts, getProductById };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,28 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const asyncHandler = require("../middlewares/asyncHandler");
-const Product = require("../models/productModel");
+const {
+  getProducts,
+  getProductById,
+} = require("../controllers/productController");
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      return res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Resource not found");
-    }
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 module.exports = router;
