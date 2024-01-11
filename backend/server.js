24541fe8b36fbc42d4327822e5353961421a09de
NancyPatel11 <nancypatel5757@gmail.com>
2024-01-11 18:56:24 +0530

    added error middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
 const connectDB = require("./config/db");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const productRouter = require("./routes/productRoutes");
 const PORT = process.env.PORT || 5000;
 
@@ -14,6 +15,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
