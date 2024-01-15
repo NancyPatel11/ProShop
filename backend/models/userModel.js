6979151f5bd33da293df986149d0c9dcfccabe02
NancyPatel11 <nancypatel5757@gmail.com>
2024-01-16 00:05:00 +0530

    register user and generate token utility

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -32,6 +32,16 @@ userSchema.methods.matchPassword = async function (enteredPass) {
   return await bcrypt.compare(enteredPass, this.password);
 };
 
+// Encrypt password using bcrypt before storing in database
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
