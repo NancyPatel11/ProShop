518d9e6c78a20adb8ba76fb0c2d1467e0e134566
NancyPatel11 <nancypatel5757@gmail.com>
2024-02-11 15:58:11 +0530

    admin order list functionality

--- frontend/src/slices/orderApiSlice.js
+++ frontend/src/slices/orderApiSlice.js
@@ -36,6 +36,13 @@ export const orderApiSlice = apiSlice.injectEndpoints({
       }),
       keepUnusedDataFor: 5,
     }),
+    getOrders: builder.query({
+      query: () => ({
+        url: `${ORDERS_URL}`,
+        method: "GET",
+      }),
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
@@ -45,4 +52,5 @@ export const {
   usePayOrderMutation,
   useGetPayPalClientIdQuery,
   useGetMyOrdersQuery,
+  useGetOrdersQuery,
 } = orderApiSlice;
