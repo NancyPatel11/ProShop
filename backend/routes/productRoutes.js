31ad7ff3fc7f88c8176cf107f1db7c52c4437112
NancyPatel11 <nancypatel5757@gmail.com>
2024-02-11 17:22:15 +0530

    admin create product with sample data

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   getProducts,
   getProductById,
+  createProduct,
 } = require("../controllers/productController");
+const { protect, admin } = require("../middlewares/authMiddleware");
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.route("/:id").get(getProductById);
 
 module.exports = router;
