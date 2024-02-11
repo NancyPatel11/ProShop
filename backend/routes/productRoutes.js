f2646ddb1f4cd10a54be18f9925f7022be73e0b5
NancyPatel11 <nancypatel5757@gmail.com>
2024-02-11 18:38:37 +0530

    admin delete product functionality

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,10 +5,15 @@ const {
   getProductById,
   createProduct,
   updateProduct,
+  deleteProduct,
 } = require("../controllers/productController");
 const { protect, admin } = require("../middlewares/authMiddleware");
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
-router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
 
 module.exports = router;
