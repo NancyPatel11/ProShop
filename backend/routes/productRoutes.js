bce8cbde78fa59b48b5c250da7b132e97e901891
NancyPatel11 <nancypatel5757@gmail.com>
2024-02-13 18:15:23 +0530

    added product review functionality

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ const {
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 } = require("../controllers/productController");
 const { protect, admin } = require("../middlewares/authMiddleware");
 
@@ -15,5 +16,6 @@ router
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 
 module.exports = router;
