7c290d04a52d28373f471686e9926a726c4b4d8c
NancyPatel11 <nancypatel5757@gmail.com>
2024-02-14 15:09:02 +0530

    validate objectid

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,14 +10,15 @@ const {
   getTopProducts,
 } = require("../controllers/productController");
 const { protect, admin } = require("../middlewares/authMiddleware");
+const checkObjectId = require("../middlewares/checkObjectId");
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.get("/top", getTopProducts);
 router
   .route("/:id")
-  .get(getProductById)
-  .put(protect, admin, updateProduct)
-  .delete(protect, admin, deleteProduct);
-router.route("/:id/reviews").post(protect, createProductReview);
+  .get(checkObjectId, getProductById)
+  .put(protect, admin, checkObjectId, updateProduct)
+  .delete(protect, admin, checkObjectId, deleteProduct);
+router.route("/:id/reviews").post(protect, checkObjectId, createProductReview);
 
 module.exports = router;
